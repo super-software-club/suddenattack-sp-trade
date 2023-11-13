4cdca1a86fe4cf1b64091d0977624fb3b3aacb66
최기환 <rlghks3004@gmail.com>
2023-11-13 22:35:36 +0900

    fix

--- src/components/root/MainNotice.tsx
+++ src/components/root/MainNotice.tsx
@@ -15,7 +15,7 @@ const MainNotice = async () => {
           backgroundImage:
             "url(https://images.unsplash.com/photo-1528722828814-77b9b83aafb2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
         }}
-        className="flex flex-row items-center justify-between w-full px-4 py-2"
+        className="flex flex-row items-center justify-between w-full px-4 py-2 rounded-t-2xl"
       >
         <h2 className="text-xl font-bold text-white">공지사항</h2>
         <Link className="text-sm font-bold text-white" href="/notice">
