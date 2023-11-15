189f788a0048d4c650b6de4699f25dab2a4c562c
최기환 <rlghks3004@gmail.com>
2023-11-16 04:57:22 +0900

    .

--- src/components/root/MainInfoLayout.tsx
+++ src/components/root/MainInfoLayout.tsx
@@ -8,7 +8,10 @@ import ReviewContainer from "./ReviewContainer";
 
 const MainInfoLayout = () => {
   // TODO: 설정에서 배터 채움 값 받아와서 채우던지 말던지 하기 0 => 채움, 1 => 채우지 않음
-  const { data: setting } = useGetSetting();
+  const { data: setting, isLoading, isError } = useGetSetting();
+
+  if (isLoading || isError) return <></>;
+
   return (
     <section
       className={`w-full lg:w-10/12 lg:px-10 lg:py-6 flex flex-col gap-6 max-w-7xl ${
