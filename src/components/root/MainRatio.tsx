9ae5df637f416ce94ef471094fa42921a4495aec
최기환 <rlghks3004@gmail.com>
2023-11-16 05:09:41 +0900

    .

--- src/components/root/MainRatio.tsx
+++ src/components/root/MainRatio.tsx
@@ -14,21 +14,27 @@ const MainRatio = () => {
       >
         <h2 className="text-xl font-bold text-white">비율 안내</h2>
       </header>
-      <div className="flex flex-col gap-4 justify-evenly items-center h-full py-6">
+      <div className="flex flex-col gap-4 justify-evenly items-start h-full py-6">
         <div className="flex flex-row gap-3 items-center">
-          <p className="text-white font-bold px-3 py-1 bg-red-500 rounded-lg">
+          <p className="text-white font-bold px-4 py-1 bg-red-400 rounded-lg border-2 border-white italic">
             구매
           </p>{" "}
-          <p className="text-white font-bold ">
-            1000SP: {setting?.buy_rate_min} ~ {setting?.buy_rate_max}
+          <p className="text-white font-bold flex flex-row items-center gap-2">
+            <span className="text-sm">1,000SP: </span>
+            <span className="text-lg">
+              {setting?.buy_rate_min} ~ {setting?.buy_rate_max}
+            </span>
           </p>
         </div>
         <div className="flex flex-row gap-3 items-center">
-          <p className="text-white font-bold px-3 py-1 rounded-lg bg-blue-500">
+          <p className="text-white font-bold px-4 py-1 rounded-lg bg-blue-400 border-2 border-white italic">
             판매
           </p>{" "}
-          <p className="text-white font-bold">
-            1000SP: {setting?.sell_rate_min} ~ {setting?.sell_rate_max}
+          <p className="text-white font-bold flex flex-row items-center gap-2">
+            <span className="text-sm">1,000SP: </span>
+            <span className="text-lg">
+              {setting?.sell_rate_min} ~ {setting?.sell_rate_max}
+            </span>
           </p>
         </div>
       </div>{" "}
